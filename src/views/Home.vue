3f658d1ea33e6df7585ce0977d7e7898083ad51a
oti <otiext@gmail.com>
2019-10-14 01:57:55 +0900

    大枠のレイアウトを作成した

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,85 @@
 <template>
-  <VContainer fluid>
-    <h1>decomoji-finder</h1>
-  </VContainer>
+  <div>
+    <VNavigationDrawer clipped fixed permanent app>
+      <VList subheader two-line>
+        <VSubheader>カテゴリー</VSubheader>
+
+        <VListTile>
+          <VListAction>
+            <VCheckbox v-model="category.basic"></VCheckbox>
+          </VListAction>
+          <VListTileContent @click="category.basic = !category.basic">
+            <VListTileTitle>Basic</VListTileTitle>
+            <VListTileSubTitle>基本セット</VListTileSubTitle>
+          </VListTileContent>
+        </VListTile>
+
+        <VListTile>
+          <VListAction>
+            <VCheckbox v-model="category.extra"></VCheckbox>
+          </VListAction>
+          <VListTileContent @click="category.extra = !category.extra">
+            <VListTileTitle>Extra</VListTileTitle>
+            <VListTileSubTitle>拡張セット</VListTileSubTitle>
+          </VListTileContent>
+        </VListTile>
+
+        <VListTile>
+          <VListAction>
+            <VCheckbox v-model="category.explicit"></VCheckbox>
+          </VListAction>
+          <VListTileContent @click="category.explicit = !category.explicit">
+            <VListTileTitle>Explicit</VListTileTitle>
+            <VListTileSubTitle>要注意セット</VListTileSubTitle>
+          </VListTileContent>
+        </VListTile>
+      </VList>
+    </VNavigationDrawer>
+    <VToolbar app clipped-left>
+      <VToolbarTitle>decomoji-finder</VToolbarTitle>
+      <VSpacer></VSpacer>
+      <VToolbarItems>
+        <VBtn flat>Goods</VBtn>
+        <VBtn flat>Patreon</VBtn>
+        <VBtn
+          flat
+          href="https://github.com/decomoji/decomoji-finder/"
+          target="_blank"
+          rel="noopener"
+          >GitHub</VBtn
+        >
+      </VToolbarItems>
+    </VToolbar>
+    <VContent>
+      <VContainer fluid fill-height>
+        <VLayout>
+          <VFLex grow>
+            <VTextField flat prepend-icon="search" label="Search" />
+          </VFLex>
+        </VLayout>
+      </VContainer>
+    </VContent>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component({})
-export default class Home extends Vue {}
+interface Category {
+  basic: boolean
+  extra: boolean
+  explicit: boolean
+}
+
+@Component
+export default class Home extends Vue {
+  /**
+   * 内部プロパティを定義する
+   */
+  category: Category = {
+    basic: true,
+    extra: true,
+    explicit: true
+  }
+}
 </script>
