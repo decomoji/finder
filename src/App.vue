0ca1c340b7ece304d240d3322e2403d46dbf1f0d
oti <otiext@gmail.com>
2019-10-14 01:57:55 +0900

    大枠のレイアウトを作成した

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <VApp>
     <RouterView role="main" class="mb-4" />
-
-    <VFooter role="contentinfo" absolute dark height="auto" class="px-3">
-      <p class="mb-0">
-        このウェブアプリは<a
-          href="https://twitter.com/otiext"
-          target="_blank"
-          rel="noopener"
-          >越智</a
-        >が作りました。
-      </p>
-      <VSpacer />
-      <p class="mb-0">
-        <a
-          href="https://github.com/decomoji/decomoji-finder/"
-          target="_blank"
-          rel="noopener"
-        >
-          GitHub
-        </a>
-      </p>
-    </VFooter>
-
     <GlobalLoading v-if="ui.hasGlobalLoadingQueue" />
   </VApp>
 </template>
