7743d8c5d4a084b10e1c3fe6d6eb8ce23fe085ac
oti <otiext@gmail.com>
2019-10-14 11:15:37 +0900

    VToolbarのヘッダーをGlobalHeaderコンポーネントに切り出した

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <VToolbar app clipped-left>
+    <VToolbarTitle>decomoji-finder</VToolbarTitle>
+    <VSpacer />
+    <VToolbarItems>
+      <VBtn
+        v-for="(item, i) in items"
+        :key="i"
+        :href="item.href"
+        :rel="item.rel"
+        :target="item.target"
+        flat
+        >{{ item.title }}</VBtn
+      >
+    </VToolbarItems>
+  </VToolbar>
+</template>
+
+<script lang="ts">
+import { Link } from '@/models/Link'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class GlobalHeader extends Vue {
+  /**
+   * 内部プロパティを定義する
+   */
+  items: Link[] = [
+    {
+      title: 'Store',
+      href: 'https://suzuri.jp/decomoji',
+      target: '_blank',
+      rel: 'noopener'
+    },
+    {
+      title: 'Patreon',
+      href: 'https://www.patreon.com/decomoji',
+      target: '_blank',
+      rel: 'noopener'
+    },
+    {
+      title: 'GitHub',
+      href: 'https://github.com/decomoji/decomoji-finder/',
+      target: '_blank',
+      rel: 'noopener'
+    }
+  ]
+}
+</script>
