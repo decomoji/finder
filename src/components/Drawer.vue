d88df33540b306d28661bb81a54315fe8a46b501
oti <otiext@gmail.com>
2019-10-14 11:46:54 +0900

    VNavigationDrawerをDrawerコンポーネントに切り出した

---
+++ src/components/Drawer.vue
@@ -0,0 +1,30 @@
+<template>
+  <VNavigationDrawer app clipped fixed permanent>
+    <VList subheader two-line>
+      <VSubheader>カテゴリー</VSubheader>
+
+      <VListTile v-for="(item, i) in items" :key="i">
+        <VListAction>
+          <VCheckbox v-model="item.selected"></VCheckbox>
+        </VListAction>
+        <VListTileContent @click="item.selected = !item.selected">
+          <VListTileTitle>{{ item.title_main }}</VListTileTitle>
+          <VListTileSubTitle>{{ item.title_sub }}</VListTileSubTitle>
+        </VListTileContent>
+      </VListTile>
+    </VList>
+  </VNavigationDrawer>
+</template>
+
+<script lang="ts">
+import { Categories } from '@/configs/Categories'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Drawer extends Vue {
+  /**
+   * 内部プロパティを定義する
+   */
+  items = Categories
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,7 @@
 <template>
   <div>
-    <VNavigationDrawer clipped fixed permanent app>
-      <VList subheader two-line>
-        <VSubheader>カテゴリー</VSubheader>
+    <Drawer />
 
-        <VListTile>
-          <VListAction>
-            <VCheckbox v-model="category.basic"></VCheckbox>
-          </VListAction>
-          <VListTileContent @click="category.basic = !category.basic">
-            <VListTileTitle>Basic</VListTileTitle>
-            <VListTileSubTitle>基本セット</VListTileSubTitle>
-          </VListTileContent>
-        </VListTile>
-
-        <VListTile>
-          <VListAction>
-            <VCheckbox v-model="category.extra"></VCheckbox>
-          </VListAction>
-          <VListTileContent @click="category.extra = !category.extra">
-            <VListTileTitle>Extra</VListTileTitle>
-            <VListTileSubTitle>拡張セット</VListTileSubTitle>
-          </VListTileContent>
-        </VListTile>
-
-        <VListTile>
-          <VListAction>
-            <VCheckbox v-model="category.explicit"></VCheckbox>
-          </VListAction>
-          <VListTileContent @click="category.explicit = !category.explicit">
-            <VListTileTitle>Explicit</VListTileTitle>
-            <VListTileSubTitle>要注意セット</VListTileSubTitle>
-          </VListTileContent>
-        </VListTile>
-      </VList>
-    </VNavigationDrawer>
     <GlobalHeader />
     <VContent>
       <VContainer fluid fill-height>
@@ -49,28 +16,15 @@
 </template>
 
 <script lang="ts">
+import Drawer from '@/components/Drawer.vue'
 import GlobalHeader from '@/components/GlobalHeader.vue'
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
-interface Category {
-  basic: boolean
-  extra: boolean
-  explicit: boolean
-}
-
 @Component({
   components: {
+    Drawer,
     GlobalHeader
   }
 })
-export default class Home extends Vue {
-  /**
-   * 内部プロパティを定義する
-   */
-  category: Category = {
-    basic: true,
-    extra: true,
-    explicit: true
-  }
-}
+export default class Home extends Vue {}
 </script>
