9025fd6d1eaaed8346e308b11978fd897a5394fb
oti <otiext@gmail.com>
2019-10-14 11:44:18 +0900

    ヘッダーのリンクリストをconfigs/HeaderLinkに切り出した

---
+++ src/configs/HeaderLinks.ts
@@ -0,0 +1,22 @@
+import { Link } from '@/models/Link'
+
+export const HeaderLinks: Link[] = [
+  {
+    title: 'Store',
+    href: 'https://suzuri.jp/decomoji',
+    target: '_blank',
+    rel: 'noopener'
+  },
+  {
+    title: 'Patreon',
+    href: 'https://www.patreon.com/decomoji',
+    target: '_blank',
+    rel: 'noopener'
+  },
+  {
+    title: 'GitHub',
+    href: 'https://github.com/decomoji/decomoji-finder/',
+    target: '_blank',
+    rel: 'noopener'
+  }
+]
