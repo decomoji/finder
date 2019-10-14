4b56c0e905855e41d4d2976de0aa73bf067b75d8
oti <otiext@gmail.com>
2019-10-14 23:56:05 +0900

    isStringとgetObjectClassNameのユーティリティを追加した

---
+++ src/utilities/isString.ts
@@ -0,0 +1,17 @@
+import { getObjectClassName } from '@/utilities/getObjectClassName'
+
+/**
+ * 値が文字列であるか否かを返す
+ * @param val
+ */
+export function isString(val: any): val is string {
+  return getObjectClassName(val) === '[object String]'
+}
+
+/**
+ * 値が空ではない文字列であるか否かを返す
+ * @param val
+ */
+export function isStringOfNotEmpty(val: any): val is string {
+  return isString(val) && val.length > 0
+}
