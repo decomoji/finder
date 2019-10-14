4dacd71e046e56fb76cb62219127e91efa28aaed
oti <otiext@gmail.com>
2019-10-14 11:47:27 +0900

    VContentをContentコンポーネントに切り出した

---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <VContent>
+    <VContainer fluid fill-height>
+      <VLayout>
+        <VFLex grow>
+          <VTextField flat prepend-icon="search" label="Search" />
+        </VFLex>
+      </VLayout>
+    </VContainer>
+  </VContent>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Content extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+@import '~vuetify/src/stylus/app'
+</style>
