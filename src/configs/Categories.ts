cbbd68e8dd90716d0ebad82ff1bcaafe09f860ed
oti <otiext@gmail.com>
2019-10-14 11:45:58 +0900

    カテゴリー情報をconfigs/Categoriesに切り出した

---
+++ src/configs/Categories.ts
@@ -0,0 +1,22 @@
+import { Category } from '@/models/Category'
+
+export const Categories: Category[] = [
+  {
+    id: 'basic',
+    title_main: 'Basic',
+    title_sub: '基本セット',
+    selected: true
+  },
+  {
+    id: 'extra',
+    title_main: 'Extra',
+    title_sub: '拡張セット',
+    selected: true
+  },
+  {
+    id: 'explicit',
+    title_main: 'Explicit',
+    title_sub: '危険セット',
+    selected: true
+  }
+]
