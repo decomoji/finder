18396b7d545da94d0f0e85477588baf23cdaeb52
oti <otiext@gmail.com>
2019-10-15 20:43:08 +0900

    UIストアのdarkを切り替えるアクションを追加した

--- src/store/modules/ui/actions.ts
+++ src/store/modules/ui/actions.ts
@@ -1,10 +1,9 @@
 import { UiActionTree } from '@/store/modules/ui/models'
 import {
   DECREMENT_GLOBAL_LOADING_QUEUE,
-  INCREMENT_GLOBAL_LOADING_QUEUE
+  INCREMENT_GLOBAL_LOADING_QUEUE,
+  TOGGLE_DARK_MODE
 } from '@/store/modules/ui/mutation-types'
-
-let screenLockTimer!: number
 
 export const actions: UiActionTree = {
   /**
@@ -23,5 +22,13 @@ export const actions: UiActionTree = {
    */
   incrementGlobalLoadingQueue({ commit }, length?) {
     commit(INCREMENT_GLOBAL_LOADING_QUEUE, length)
+  },
+
+  /**
+   * ダークモードをトグルする
+   * @param commit
+   */
+  toggleDarkMode({ commit }) {
+    commit(TOGGLE_DARK_MODE)
   }
 }
