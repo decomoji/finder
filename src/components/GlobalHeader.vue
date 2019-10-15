1e9170e04d16e6916f33339da100008d5d10eb3b
oti <otiext@gmail.com>
2019-10-15 23:57:29 +0900

    GlobalHeaderコンポーネントにテキストフィールドを追加して検索クエリのゲッターとアクションを引き当てた

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,15 @@
 <template>
-  <VToolbar app clipped-left dark color="primary">
-    <VToolbarTitle>decomoji-finder</VToolbarTitle>
+  <VToolbar app clipped-left dark color="primary" class="GlobalHeader">
+    <VToolbarTitle class="__title">decomoji-finder v1</VToolbarTitle>
+    <VTextField
+      :value="ui.searchQuery"
+      flat
+      hide-details
+      solo-inverted
+      prepend-inner-icon="search"
+      label="Search"
+      @input="updateSearchQuery"
+    />
     <VSpacer />
     <VToolbarItems>
       <VBtn
@@ -18,13 +27,31 @@
 
 <script lang="ts">
 import { HeaderLinks } from '@/configs/HeaderLinks'
+import { UiActionDispatchers, UiViewModel } from '@/store/modules/ui/models'
 import { Component, Vue } from 'vue-property-decorator'
+import { Action, Getter } from 'vuex-class'
 
 @Component
 export default class GlobalHeader extends Vue {
+  // viewModel を引き当てる
+  @Getter('ui/viewModel') ui!: UiViewModel
+
+  /**
+   * アクションを引き当てる
+   */
+  @Action('ui/updateSearchQuery')
+  updateSearchQuery!: UiActionDispatchers['updateSearchQuery']
+
   /**
    * 内部プロパティを定義する
    */
   items = HeaderLinks
 }
 </script>
+<style lang="stylus" scoped>
+@import '~vuetify/src/stylus/settings/_variables'
+
+.GlobalHeader
+  .__title
+    width: 300px
+</style>
