ff677ebae328d483a679912a2685c78d04f66267
oti <otiext@gmail.com>
2019-10-15 23:09:58 +0900

    Drawerコンポーネントにui.categoryのゲッターとアクションを引き当てた

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -15,9 +15,9 @@
       <VSubheader>カテゴリー</VSubheader>
       <VListTile v-for="(item, i) in items" :key="i">
         <VListTileAction>
-          <VCheckbox v-model="item.selected" />
+          <VCheckbox v-model="ui.category[item.id]" />
         </VListTileAction>
-        <VListTileContent @click="item.selected = !item.selected">
+        <VListTileContent @click="handleClickCategory(item.id)">
           <VListTileTitle>{{ item.title_main }}</VListTileTitle>
           <VListTileSubTitle>{{ item.title_sub }}</VListTileSubTitle>
         </VListTileContent>
@@ -28,6 +28,7 @@
 
 <script lang="ts">
 import { Categories } from '@/configs/Categories'
+import { CategoryId } from '@/models/CategoryId'
 import { UiActionDispatchers, UiViewModel } from '@/store/modules/ui/models'
 import { Component, Vue } from 'vue-property-decorator'
 import { Action, Getter } from 'vuex-class'
@@ -40,6 +41,8 @@ export default class Drawer extends Vue {
   /**
    * アクションを引き当てる
    */
+  @Action('ui/toggleCategory')
+  toggleCategory!: UiActionDispatchers['toggleCategory']
   @Action('ui/toggleDarkMode')
   toggleDarkMode!: UiActionDispatchers['toggleDarkMode']
 
@@ -47,6 +50,14 @@ export default class Drawer extends Vue {
    * 内部プロパティを定義する
    */
   items = Categories
+
+  /**
+   * @listens VListTileContent@click
+   * @listens VCheckbox[v-model=ui.category[id]]@click
+   */
+  handleClickCategory(categoryId: CategoryId) {
+    this.toggleCategory(categoryId)
+  }
 
   /**
    * @listens VListTileContent@click
