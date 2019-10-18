ba05cca7fabbb35e412fa4fb814031bfff2e4716
oti <otiext@gmail.com>
2019-10-19 01:59:09 +0900

    アイコンサイズに関連する型定義とconfigsを追加した

---
+++ src/configs/DrawerIconSizeList.ts
@@ -0,0 +1,16 @@
+import { IconSizeItem } from '@/models/IconSizeItem'
+
+export const DrawerIconSizeList: IconSizeItem[] = [
+  {
+    label: '64px',
+    value: 'l'
+  },
+  {
+    label: '32px',
+    value: 'm'
+  },
+  {
+    label: '16px',
+    value: 's'
+  }
+]
