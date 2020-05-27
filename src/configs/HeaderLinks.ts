4431e7f5db9f09dc61c607f8d56d41cd40d0a4a3
oti <otiext@gmail.com>
2020-05-28 01:31:21 +0900

    ヘッダーのリンクを GitHub だけにした

--- src/configs/HeaderLinks.ts
+++ src/configs/HeaderLinks.ts
@@ -1,24 +1,24 @@
 import { LinkItem } from '@/models/LinkItem'
 
 export const HeaderLinks: LinkItem[] = [
-  {
-    title: "What's decomoji?",
-    href: 'https://github.com/decomoji/slack-reaction-decomoji',
-    target: '_blank',
-    rel: 'noopener'
-  },
-  {
-    title: 'Store',
-    href: 'https://suzuri.jp/decomoji',
-    target: '_blank',
-    rel: 'noopener'
-  },
-  {
-    title: 'Patreon',
-    href: 'https://www.patreon.com/decomoji',
-    target: '_blank',
-    rel: 'noopener'
-  },
+  // {
+  //   title: "What's decomoji?",
+  //   href: 'https://github.com/decomoji/slack-reaction-decomoji',
+  //   target: '_blank',
+  //   rel: 'noopener'
+  // },
+  // {
+  //   title: 'Store',
+  //   href: 'https://suzuri.jp/decomoji',
+  //   target: '_blank',
+  //   rel: 'noopener'
+  // },
+  // {
+  //   title: 'Patreon',
+  //   href: 'https://www.patreon.com/decomoji',
+  //   target: '_blank',
+  //   rel: 'noopener'
+  // },
   {
     title: 'GitHub',
     href: 'https://github.com/decomoji/decomoji-finder/',
