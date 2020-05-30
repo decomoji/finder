43bbb34a06be8480a72ec6629f51ba82e7858a26
oti <otiext@gmail.com>
2020-05-30 18:30:30 +0900

    ヘッダーのロゴと検索窓部分を再実装した

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,45 @@
 <template>
-  <div app clipped-left dark class="Header">
-    <div class="__title">decomoji-finder</div>
-    <input
-      type="text"
-      :input-value="ui.searchQuery"
-      clearable
-      flat
-      hide-details
-      solo-inverted
-      class="__input"
-      prepend-inner-icon="search"
-      label="Search"
-      @input="debounceUpdateSearchQuery($event)"
-    />
+  <div class="Header px-4 divide-y divide-white divide-opacity-25">
+    <div class="flex items-center py-2">
+      <div class="flex w-1/2">
+        <div class="pr-4">
+          <a class="text-2xl text-white" href="/">decomoji-finder</a>
+        </div>
+
+        <div class="flex items-center">
+          <label class="pr-2 text-white leading-none" for="search">
+            <Icon value="search" />
+            <span class="sr-only">検索する</span>
+          </label>
+          <input
+            id="search"
+            class="__search flex-auto rounded px-4 px-2 w-auto h-10 bg-white bg-opacity-75 focus:bg-white focus:outline-none"
+            type="text"
+            :value="ui.searchQuery"
+            @input="debounceUpdateSearchQuery($event)"
+          />
+        </div>
+      </div>
+
+      <div class="flex w-1/2"></div>
+    </div>
+
+    <div class="flex items-center py-2"></div>
   </div>
 </template>
 
 <script lang="ts">
+import Icon from "@/components/Icon.vue";
 import { HeaderLinks } from "@/configs/HeaderLinks";
 import { UiActions, UiViewModel } from "@/store/modules/ui/models";
 import { Component, Vue } from "vue-property-decorator";
 import { Action, Getter } from "vuex-class";
 
-@Component
+@Component({
+  components: {
+    Icon
+  }
+})
 export default class Header extends Vue {
   // viewModel を引き当てる
   @Getter("ui/viewModel") ui!: UiViewModel;
@@ -52,14 +69,11 @@ export default class Header extends Vue {
 </script>
 <style lang="sass" scoped>
 .Header
-  .theme--light &
-    background-color: #290829
-  .theme--dark &
+  background-color: #290829
+  .-dark &
     background-color: #0f0e12
-  .__title
-    padding-right: 24px
-  .__input
-    max-width: 450px
-  .__link
-    text-transform: none
+
+  .__search
+    &:focus
+      box-shadow: 0 0 0 4px rgba(255,255,255, 0.5)
 </style>
