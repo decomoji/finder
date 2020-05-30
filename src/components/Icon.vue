9f350d8273c472abe4bac58d16c9330ace2ce077
oti <otiext@gmail.com>
2020-05-30 18:24:25 +0900

    material-icons を引き当てる <Icon /> コンポーネントを追加した

---
+++ src/components/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <i class="material-icons" aria-hidden="true">{{ value }}</i>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from "vue-property-decorator";
+
+export default class Icon extends Vue {
+  @Prop({ required: true }) value!: string;
+}
+</script>
+
+<style lang="sass" scoped>
+.material-icons
+  line-heihgt: 1
+</style>
