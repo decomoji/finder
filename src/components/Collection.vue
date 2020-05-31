084ec2999f1a75e1ddfa2cd1c98d9588206d5c3e
oti <otiext@gmail.com>
2020-05-31 18:31:23 +0900

    Collection.vue を Tailwind に寄せた

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,51 +1,30 @@
 <template>
   <div
-    :value="value"
-    :mini-variant.sync="shrink"
-    :mini-variant-width="width"
-    app
-    fixed
-    permanent
-    right
-    class="Collection"
+    v-show="shows"
+    class="Collection fixed inset-x-0 bottom-0 p-4 overflow-y-auto scrolling-touch"
   >
-    <div class="__scrollable">
-      <div>
-        <div>
-          <div>
-            <button icon @click.stop="shrink = !shrink">
-              <div>{{ expandIconName }}</div>
-            </button>
-          </div>
-          <div>
-            <div>コレクション</div>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div>
-        <div v-if="!shrink && collection.items.length > 0">
-          <span class="body-1"
-            >ダブルクリックするか delete キーでコレクションから外せます。</span
-          >
-        </div>
-        <div :class="['__list', `-${ui.iconSize}`]">
-          <button
-            v-for="(item, i) in collection.items"
-            :key="`${item.name}_${item.category}_${i}`"
-            :class="['__item', `-${ui.iconSize}`]"
-            @dblclick="handleDbclickItem(item)"
-            @keydown.delete="handleDeleteItem(item)"
-          >
-            <img
-              :alt="item.name"
-              :src="`/decomoji/${item.category}/${item.name}.png`"
-              :class="`__icon -${ui.iconSize}`"
-              width="64"
-            />
-          </button>
-        </div>
-      </div>
+    <p class="text-base">
+      ダブルクリックするか delete キーでコレクションから外せます。
+    </p>
+    <div :class="['__list', 'grid grid-flow-row', `-${ui.iconSize}`]">
+      <button
+        v-for="(item, i) in collection.items"
+        :key="`${item.name}_${item.category}_${i}`"
+        :class="[
+          '__item',
+          'p-1 rounded-md border border-solid border-transparent text-center leading-none focus:outline-none',
+          `-${ui.iconSize}`
+        ]"
+        @dblclick="removeItem(item)"
+        @keydown.delete="removeItem(item)"
+      >
+        <img
+          :alt="item.name"
+          :class="['__icon block m-auto w-full', `-${ui.iconSize}`]"
+          :src="`/decomoji/${item.category}/${item.name}.png`"
+          width="64"
+        />
+      </button>
     </div>
   </div>
 </template>
@@ -82,39 +61,13 @@ export default class Collection extends Vue {
    */
   @Prop() query!: QueryObject;
 
-  shrink = true;
-  value = true;
-
-  /**
-   * @get - ドロワーを閉じたときの最小幅を返す
-   */
-  get width() {
-    return window.innerWidth - document.documentElement.clientWidth ? 100 : 84;
+  // @get - コレクションを表示するか否かを返す
+  get shows() {
+    return this.collection.items.length > 0;
   }
 
   /**
-   * @get - ドロワーの開閉ボタンのアイコン名を返す
-   */
-  get expandIconName() {
-    return this.shrink ? "keyboard_arrow_left" : "keyboard_arrow_right";
-  }
-
-  /**
-   * @listens - button.dbclick
-   */
-  handleDbclickItem(item: DecomojiCollectionItem) {
-    this.removeItem(item);
-  }
-
-  /**
-   * @listens - button.keydown
-   */
-  handleDeleteItem(item: DecomojiCollectionItem) {
-    this.removeItem(item);
-  }
-
-  /**
-   * @method
+   * @method - コレクションからアイテムを削除する
    */
   removeItem(item: DecomojiCollectionItem) {
     this.remove(item);
@@ -126,9 +79,9 @@ export default class Collection extends Vue {
     );
   }
 
-  handleClickDownloadManagerList() {}
-  handleClickDownloadAliasList() {}
-
+  /**
+   * @lifecyle
+   */
   created() {
     // パラメータをパースしてコレクションに追加する
     const { basic, extra, explicit, preview } = this.query || {};
@@ -160,54 +113,37 @@ export default class Collection extends Vue {
 
 <style lang="sass" scoped>
 .Collection
-  .theme--light &
-    background-color: #f4f4f4
-  .theme--dark &
+  background-color: #f4f4f4
+  .-dark &
     background-color: #1a1c20
-  .__scrollable
-    height: 100%
-    overflow-y: auto
   .__list
-    display: grid
-    grid-auto-rows: auto
-    padding: 10px
-    overflow-x: hidden
     &.-l
-      gap: 10px
+      @apply .gap-3
       grid-template-columns: repeat(auto-fill, minmax(64px, 1fr))
     &.-m
-      gap: 5px
+      @apply .gap-2
       grid-template-columns: repeat(auto-fill, minmax(32px, 1fr))
     &.-s
-      gap: 3px
+      @apply .gap-1
       grid-template-columns: repeat(auto-fill, minmax(16px, 1fr))
 
   .__item
-    padding: 5px
-    border: 1px solid transparent
-    border-radius: 4px
-    line-height: 1
-    text-align: center
     transition: box-shadow 0.03s ease-out
 
     &.-s
-      padding: 0
+      @apply .p-0
 
-    .theme--light &
+    .-light &
       background-color: #ffffff
-    .theme--dark &
+    .-dark &
       background-color: #15171a
 
     &:focus
-      outline: 0;
-      .theme--light &
-        box-shadow: inset 0 0 0 4px #adbfca
-      .theme--dark &
-        box-shadow: inset 0 0 0 4px #5c7280
+      box-shadow: 0 0 0 4px #adbfca
+      .-dark &
+        box-shadow: 0 0 0 4px #5c7280
 
   .__icon
-    width: 100%
-    vertical-align: top
     &.-l
       max-width: 64px
     &.-m
