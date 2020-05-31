cf00a7d22e21ec29142fa8a243874a23b0e41045
oti <otiext@gmail.com>
2020-06-01 01:34:40 +0900

    purgecss をやめた

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === "production",
-    content: ["components/**/*.vue", "views/**/*.vue", "vue.config.js"]
-  },
+  purge: [],
   theme: {
     extend: {}
   },
