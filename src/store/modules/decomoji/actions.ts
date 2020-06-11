f2da92acbfbb12bf44e5954554565c7a94397cbb
oti <otiext@gmail.com>
2020-06-11 13:06:30 +0900

    decomoji ストアにコレクション操作の Action を追加した

--- src/store/modules/decomoji/actions.ts
+++ src/store/modules/decomoji/actions.ts
@@ -1,9 +1,14 @@
+import { CategoryName } from "@/models/CategoryName";
 import { RootState } from "@/store/models";
 import {
   DecomojiActionPayloads as ThisActionPayloads,
   DecomojiState as ThisState
 } from "./models";
 import {
+  ADD_TO_COLLECTION,
+  CLEAR_COLLECTION,
+  REMOVE_FROM_COLLECTION,
+  RECEIVE_COLLECTION,
   TOGGLE_CATEGORY,
   TOGGLE_DARK_MODE,
   TOGGLE_NAME_SHOWS,
@@ -15,6 +20,72 @@ import { isStringOfNotEmpty } from "@/utilities/isString";
 import { ActionTree } from "vuex";
 
 export const actions: ActionTree<ThisState, RootState> = {
+  /**
+   * 選択した要素を選択済みデコモジリストに加える
+   * @param commit
+   * @param payload
+   */
+  add({ commit }, payload: ThisActionPayloads["add"]) {
+    commit(ADD_TO_COLLECTION, payload);
+  },
+
+  /**
+   * 選択済みデコモジリストを空にする
+   * @param commit
+   * @param payload
+   */
+  clear({ commit }) {
+    commit(CLEAR_COLLECTION);
+  },
+
+  /**
+   * 選択解除した要素を選択済みデコモジリストから削除する
+   * @param commit
+   * @param payload
+   */
+  remove({ commit }, payload: ThisActionPayloads["remove"]) {
+    commit(REMOVE_FROM_COLLECTION, payload);
+  },
+
+  /**
+   * 選択済みデコモジリストを受領する
+   * @param commit
+   * @param payload
+   */
+  receive({ commit }, payload: ThisActionPayloads["receive"]) {
+    // パラメータをパースしてコレクションに追加する
+    const { basic, extra, explicit, preview } = payload || {};
+    const _basic = isStringOfNotEmpty(basic)
+      ? basic
+          .split(",")
+          .map((name: string) => ({ name, category: "basic" as CategoryName }))
+      : [];
+    const _extra = isStringOfNotEmpty(extra)
+      ? extra
+          .split(",")
+          .map((name: string) => ({ name, category: "extra" as CategoryName }))
+      : [];
+    const _explicit = isStringOfNotEmpty(explicit)
+      ? explicit.split(",").map((name: string) => ({
+          name,
+          category: "explicit" as CategoryName
+        }))
+      : [];
+    const _preview = isStringOfNotEmpty(preview)
+      ? preview.split(",").map((name: string) => ({
+          name,
+          category: "preview" as CategoryName
+        }))
+      : [];
+
+    commit(RECEIVE_COLLECTION, [
+      ..._basic,
+      ..._extra,
+      ..._explicit,
+      ..._preview
+    ]);
+  },
+
   /**
    * カテゴリーをトグルする
    * @param commit
