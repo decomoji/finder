d44e82750943333b9f785fe08fae085820c4ce96
oti <otiext@gmail.com>
2020-06-11 23:50:42 +0900

    Main.vue に virtual-scroller を追加した

--- src/configs/AvailableDecomojis.ts
+++ src/configs/AvailableDecomojis.ts
@@ -1,12 +1,19 @@
 import { DecomojiBasic } from "@/configs/DecomojiBasic";
 import { DecomojiExplicit } from "@/configs/DecomojiExplicit";
 import { DecomojiExtra } from "@/configs/DecomojiExtra";
-import { DecomojiName } from "@/models/DecomojiName";
 import { DecomojiPreview } from "@/configs/DecomojiPreview";
 
-export const AvailableDecomojis = Object.freeze({
-  basic: DecomojiBasic as DecomojiName[],
-  extra: DecomojiExtra as DecomojiName[],
-  explicit: DecomojiExplicit as DecomojiName[],
-  preview: DecomojiPreview as DecomojiName[]
-});
+const basics = DecomojiBasic.map(name => ({ name, category: "basic" }));
+const extras = DecomojiExtra.map(name => ({ name, category: "extra" }));
+const explicits = DecomojiExplicit.map(name => ({
+  name,
+  category: "explicit"
+}));
+const previews = DecomojiPreview.map(name => ({ name, category: "preview" }));
+
+export const AvailableDecomojis = [
+  ...basics,
+  ...extras,
+  ...explicits,
+  ...previews
+].map((item, index) => ({ ...item, ...{ id: index } }));
