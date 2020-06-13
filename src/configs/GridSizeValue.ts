7db596a2833a66e40148307565cf9363b4a4a2d6
oti <otiext@gmail.com>
2020-06-13 17:19:05 +0900

    GridSizeValue の定数ファイルを追加した

---
+++ src/configs/GridSizeValue.ts
@@ -0,0 +1,19 @@
+import { StringKeyNumberValueSignature } from "@/models/StringKeyNumberValueSignature";
+
+export const GridContainerPaddingValue: StringKeyNumberValueSignature = {
+  l: 10,
+  m: 5,
+  s: 3
+};
+
+export const GridItemGapValue: StringKeyNumberValueSignature = {
+  l: 10,
+  m: 5,
+  s: 3
+};
+
+export const GridMinItemWidthValue: StringKeyNumberValueSignature = {
+  l: 128,
+  m: 42,
+  s: 24
+};
