4e48370b30874e4248b35b0f5ec739053f5dfed5
oti <otiext@gmail.com>
2020-07-05 13:53:44 +0900

    feat(configs): main.vue で利用する CSS Grid 向けの辞書に 'll' 用のサイズを追加した

--- src/configs/GridSizeValue.ts
+++ src/configs/GridSizeValue.ts
@@ -1,28 +1,28 @@
 import { StringKeyNumberValueSignature } from "@/models/StringKeyNumberValueSignature";
 
 export const GridContainerPaddingValue: StringKeyNumberValueSignature = {
-  namedL: 10,
+  ll: 10,
   l: 8,
   m: 5,
   s: 3,
 };
 
 export const GridItemGapValue: StringKeyNumberValueSignature = {
-  namedL: 10,
+  ll: 10,
   l: 8,
   m: 5,
   s: 3,
 };
 
 export const GridMinItemWidthValue: StringKeyNumberValueSignature = {
-  namedL: 128,
+  ll: 128,
   l: 80,
   m: 42,
   s: 24,
 };
 
 export const GridRowHeightValue: StringKeyNumberValueSignature = {
-  namedL: 139,
+  ll: 139,
   l: 88,
   m: 50,
   s: 28,
