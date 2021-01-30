77daeb4ae5f48a3104bec4268d5f45e9bea3006f
oti <otiext@gmail.com>
2021-01-30 17:49:17 +0900

    chore(utilities): npm run lint

--- src/configs/DisplayVersionList.ts
+++ src/configs/DisplayVersionList.ts
@@ -1,11 +1,9 @@
 import { AvailableVersions } from "@/configs/AvailableVersions";
 import { VersionItem } from "@/models/VersionItem";
 
-export const DisplayVersionList: VersionItem[] = AvailableVersions.map<
-  VersionItem
->((value: string) => ({
-  text: value,
-  value,
-})).sort((a, b) =>
-  a.value.localeCompare(b.value, undefined, { numeric: true })
-);
+export const DisplayVersionList: VersionItem[] = AvailableVersions.map<VersionItem>(
+  (value: string) => ({
+    text: value,
+    value,
+  })
+).sort((a, b) => a.value.localeCompare(b.value, undefined, { numeric: true }));
