81c2a3a3a7e0fd7b97c1f6361449d3117ae3cae5
oti <otiext@gmail.com>
2023-10-03 18:20:39 +0900

    feat: src/components/OptionSelector.vue, details に name 属性を追加した

--- src/components/OptionSelector.vue
+++ src/components/OptionSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <details class="Detail">
+  <details class="Detail" name="selectors">
     <summary class="__summary">オプション</summary>
     <div class="__panel -option">
       <label class="__label">
