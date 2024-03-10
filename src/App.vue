e6e1454ca85812280cf3a82fbd3aad4a9e930194
oti <otiext@gmail.com>
2024-03-10 21:59:26 +0900

    chore: npx prettier --write .

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="sticky top-0 left-0 p-[--paddingHeader] w-full text-[--colorHeader] bg-[--bgHeader] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_8px_8px_rgba(0,0,0,0.075)]">
+  <div
+    class="sticky top-0 left-0 flex items-center p-[--paddingHeader] w-full text-[--colorHeader] bg-[--bgHeader] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_8px_8px_rgba(0,0,0,0.075)]"
+  ></div>
 
-  </div>
+  <main></main>
 
-  <main>
-
-  </main>
-
-  <footer class="sticky top-0 left-0 flex items-center p-[--paddingFooter] w-full text-[--colorFooter] bg-[--bgFooter] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_-8px_8px_rgba(0,0,0,0.075)]">
+  <footer
+    class="sticky top-0 left-0 flex items-center p-[--paddingFooter] w-full text-[--colorFooter] bg-[--bgFooter] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_-8px_8px_rgba(0,0,0,0.075)]"
+  >
     <p>
-      このウェブアプリは <a
+      このウェブアプリは
+      <a
         href="https://twitter.com/otiext"
         class="link:text-inherit visited:text-inherit hover:text-inherit active:text-inherit"
         >oti</a
-      > が作りました。
+      >
+      が作りました。
     </p>
     <p class="ml-auto">
       <a
