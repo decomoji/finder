1d38217d4e5a5c32ccaff25373443aaefd163239
oti <otiext@gmail.com>
2024-03-10 21:45:05 +0900

    feat: フッターの中身を整えた

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,20 @@
 
   </main>
 
-
-  <footer class="sticky flex items-center top-0 left-0 p-[--paddingFooter] w-full text-[--colorFooter] bg-[--bgFooter] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_-8px_8px_rgba(0,0,0,0.075)]">
+  <footer class="sticky top-0 left-0 flex items-center p-[--paddingFooter] w-full text-[--colorFooter] bg-[--bgFooter] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_-8px_8px_rgba(0,0,0,0.075)]">
+    <p>
+      このウェブアプリは <a
+        href="https://twitter.com/otiext"
+        class="link:text-inherit visited:text-inherit hover:text-inherit active:text-inherit"
+        >oti</a
+      > が作りました。
+    </p>
+    <p class="ml-auto">
+      <a
+        href="https://github.com/decomoji/finder/"
+        class="link:text-inherit visited:text-inherit hover:text-inherit active:text-inherit"
+        >GitHub</a
+      >, MIT License.
+    </p>
   </footer>
 </template>
