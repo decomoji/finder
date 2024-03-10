6c03fcd2f6b96942ad2e416546a821751d76548c
oti <otiext@gmail.com>
2024-03-10 21:39:23 +0900

    feat: src/App.vue, ヘッダーのベースを Tailwind 化した

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <input type="text">
+  <div class="sticky top-0 left-0 p-[--paddingHeader] w-full text-[--colorHeader] bg-[--bgHeader] shadow-[0_2px_4px_rgba(0,0,0,0.15),0_8px_8px_rgba(0,0,0,0.075)]">
+
   </div>
 
   <main>
