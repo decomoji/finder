186d2ba7585d90568bc9d8786eba62b889ab7399
oti <otiext@gmail.com>
2024-03-16 18:33:21 +0900

    feat: src/App.vue, 全選択ボタンに disabled 条件を追加した

--- src/App.vue
+++ src/App.vue
@@ -422,6 +422,7 @@ const items = ref(filteredDecomojis)
             </label>
             <button
               class="flex justify-center items-center p-[calc(var(--space-xs)/2)_var(--space-xs)] disabled:text-[rgba(255,255,255,0.25)]"
+              :disabled="Object.values(state.version).every(v => v)"
               type="button"
               @click="availableVersions.forEach(value => state.version[value] = true)"
             >
